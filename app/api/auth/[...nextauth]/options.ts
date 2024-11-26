8f6edf2afc6b7b25ba06b481cc431d6bf9a8fa81
abhraneeldhar <abhraneeldhar@gmail.com>
2024-11-26 15:34:09 +0530

    supabase adapter working babayy

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -4,37 +4,45 @@ import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
 import Github from "next-auth/providers/github"
 import { mongoClientCS } from "@/app/utils/mongoConnector"
+import { SupabaseAdapter } from "@auth/supabase-adapter"
+import jwt from "jsonwebtoken"
 import { userType } from "@/app/utils/fileFormat"
 import { v4 as uuidv4 } from "uuid";
+
+
 export const options: NextAuthOptions={
+    adapter: SupabaseAdapter({
+        url: process.env.SUPABASE_URL as string,
+        secret: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+      }),
     providers:[
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string
-        }),
-        CredentialsProvider({
-            name: "Credentails",
-            credentials:{
-                username:{
-                    label:"Username: ",
-                    type: "text",
-                    placeholder: "Your username...",
-                },
-                password: {
-                    label: "Password: ",
-                    type: "password"
-                }
-            },
-            async authorize(credentials){
-                const user={id:"42", name:"Abhraneel", password:"nextauth"}
-                if(credentials?.username == user.name && credentials?.password==user.password){
-                    return user
-                }
-                else{
-                    return null
-                }
-            }
-        }),
+        // GitHubProvider({
+        //     clientId: process.env.GITHUB_ID as string,
+        //     clientSecret: process.env.GITHUB_SECRET as string
+        // }),
+        // CredentialsProvider({
+        //     name: "Credentails",
+        //     credentials:{
+        //         username:{
+        //             label:"Username: ",
+        //             type: "text",
+        //             placeholder: "Your username...",
+        //         },
+        //         password: {
+        //             label: "Password: ",
+        //             type: "password"
+        //         }
+        //     },
+        //     async authorize(credentials){
+        //         const user={id:"42", name:"Abhraneel", password:"nextauth"}
+        //         if(credentials?.username == user.name && credentials?.password==user.password){
+        //             return user
+        //         }
+        //         else{
+        //             return null
+        //         }
+        //     }
+        // }),
         GoogleProvider({
             clientId: process.env.GOOGLE_ID as string,
             clientSecret: process.env.GOOGLE_SECRET as string
@@ -68,9 +76,24 @@ export const options: NextAuthOptions={
             catch (error){
                 console.log(error)
             }
-            return "/allnotes";
+            // return "/allnotes";
+            return true;
         },
+        async session({ session, user }) {
+            const signingSecret = process.env.SUPABASE_JWT_SECRET as string
+            
+            if (signingSecret && user && session) {
+              const payload = {
+                aud: "authenticated",
+                exp: Math.floor(new Date(session.expires).getTime() / 1000),
+                sub: user.id,
+                email: user.email,
+                role: "authenticated",
+              }
+              session.supabaseAccessToken = jwt.sign(payload, signingSecret)
+            }
+            return session
+          },
     },
-    
     
 }
--- app/setupAccount/page.tsx
+++ app/setupAccount/page.tsx
@@ -11,11 +11,52 @@ import pfp from "./assets/meow.jpg"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@radix-ui/themes"
-import { X, User, AtSign } from "lucide-react"
+import { X, User, AtSign, Camera } from "lucide-react"
+import { ChangeEvent, useEffect, useReducer, useRef, useState } from "react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { AlertCircle } from "lucide-react"
 
 
+
+function isAlphaNumeric(str: string) {
+    return !(/^[a-zA-Z0-9\s]*$/.test(str));
+}
+function isNameLongOrShort(str: string) {
+    if (str.length > 20 || str.length <= 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 export default function SetupAccount() {
+    const [nameState, setNameState] = useState("");
+    const [usernameState, setUsernameState] = useState("")
+    const [pfpState, setPfpState] = useState<string>()
+
+
+    const [nameAlert, setNameAlert] = useState(false)
+    const [nameLengthAlert, setNameLengthAlert] = useState(false)
+    const [usernameAlert, setUsernameAlert] = useState(false)
+    const [usernameLengthAlert, setUsernameLengthAlert] = useState(false)
+
+    const imageInputRef = useRef<HTMLInputElement>(null)
+
+    const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            console.log(e.target.files[0])
+            const file = e.target.files[0];
+            const url = URL.createObjectURL(file)
+            setPfpState(url)
+        }
+    }
+
+    useEffect(() => {
+        console.log(pfpState)
+    }, [pfpState])
+
+
     return (<>
         <div className={styles.main}>
             <div className={styles.container}>
@@ -31,23 +72,73 @@ export default function SetupAccount() {
                     <div className={styles.appLogo}>
                         <Image src={appLogo} alt="Nebula" />
                         X
-                        <Image src={pfp} alt="pfp" />
+                        {/* <div className={styles.pfpHolder}> */}
+                        <Image src={pfpState || pfp} height={0} width={0} alt="pfp" onClick={() => {
+                            imageInputRef.current?.click();
+                            console.log("img")
+                        }} />
+                        {/* </div> */}
                     </div>
 
+
+
+                    <input ref={imageInputRef} id="pfp" name="pfp" type="file" accept="image/*" className={styles.imageInput} onChange={handleImage} />
 
                     <form onSubmit={(e) => {
                         e.preventDefault();
                         console.log("submitted");
                     }} className={styles.formContainer}>
 
+                        <div className={styles.cameraIconHolder}>
+                            <Camera onClick={()=>{imageInputRef.current?.click();}}/>
+                        </div>
                         <label htmlFor="name"><User />Name</label>
-                        <input name="name" type="text" placeholder="name" />
+                        <input name="name" id="name" type="text" placeholder="name" onChange={(e) => {
+                            setNameState(e.target.value);
+                            setNameAlert(isAlphaNumeric(e.target.value));
+                            setNameLengthAlert(isNameLongOrShort(e.target.value));
+                        }} />
 
                         <label htmlFor="username"><AtSign />Username</label>
-                        <input name="username" type="text" placeholder="username" />
+                        <input name="username" id="username" type="text" placeholder="username" onChange={(e) => {
+                            setUsernameState(e.target.value);
+                            setUsernameAlert(isAlphaNumeric(e.target.value));
+                            setUsernameLengthAlert(isNameLongOrShort(e.target.value));
+                        }} />
 
-                        <Button type="submit">Configure</Button>
+                        <Button disabled={nameAlert || nameLengthAlert || usernameAlert || usernameLengthAlert}>Configure</Button>
                     </form>
+
+
+                    {nameAlert && (<>
+                        <Alert className={styles.alert} variant="destructive">
+                            <AlertDescription>
+                                Name can only contain alphanumeric charecters.
+                            </AlertDescription>
+                        </Alert>
+                    </>)}
+                    {nameLengthAlert && (<>
+                        <Alert className={styles.alert} variant="destructive">
+                            <AlertDescription>
+                                Name can't be longer than 20 charecters.
+                            </AlertDescription>
+                        </Alert>
+                    </>)}
+                    {usernameAlert && (<>
+                        <Alert className={styles.alert} variant="destructive">
+                            <AlertDescription>
+                                Username can only contain alphanumeric charecters.
+                            </AlertDescription>
+                        </Alert>
+                    </>)}
+                    {usernameLengthAlert && (<>
+                        <Alert className={styles.alert} variant="destructive">
+                            <AlertDescription>
+                                Username can't be longer than 20 charecters.
+                            </AlertDescription>
+                        </Alert>
+                    </>)}
+
 
                 </div>
             </div>
