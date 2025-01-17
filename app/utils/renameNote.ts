a39038c96ec22dfb2fbfbc22c0839a58fd5311f3
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-17 15:15:56 +0530

    removed api routes temporarily

--- app/utils/renameNote.ts
+++ app/utils/renameNote.ts
@@ -1,6 +1,10 @@
-export async function renameNote(noteId:string,newName:string){
-    const response = await fetch(process.env.NEXT_PUBLIC_URL+`/api/renameNote?noteId=${noteId}&newName=${newName}`, {
-        method: 'PATCH'
-    });
-    console.log(response)
+"use server"
+import { mongoClientCS } from "./mongoConnector";
+
+export async function renameNote(noteId: string, newName: string) {
+    await mongoClientCS.connect();
+    const db = mongoClientCS.db("notesApp");
+    const notesCollection = db.collection("notes");
+    const upRes = await notesCollection.updateOne({ id: noteId }, { $set: { title: newName } });
+    return (upRes)
 }
