c324e4aef2521d09b2137c6f00410fc6453c5f76
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-17 17:47:46 +0530

    supabase default fetching works

--- app/utils/setupNewAccount.ts
+++ app/utils/setupNewAccount.ts
@@ -1,13 +1,19 @@
 "use server"
+import { createClient } from "@supabase/supabase-js";
 import { getUserDetails } from "./getUserDetails";
 import { mongoClientCS } from "./mongoConnector";
 import { v4 as uuidv4 } from "uuid";
 
 
-export async function setupNewAccount(userId:string){
-    const userDetails= await getUserDetails(userId);
-    if(userDetails.newAccount==false){
-        return(null)
+export async function setupNewAccount(userId: string) {
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    const userDetails = await getUserDetails(userId);
+    if (userDetails.newAccount == false) {
+        return (null)
     }
 
     await mongoClientCS.connect();
@@ -20,12 +26,19 @@ export async function setupNewAccount(userId:string){
         const { _id, ...rest } = note;
         return {
             ...rest,
-            id:uuidv4(),
+            id: uuidv4(),
             owner: userId,
             lastModifiedAt: Date.now()
         };
     });
 
     console.log(newDefaultNotes);
+
+    const { data:defaultProfilePics, error } = await supabase
+        .storage
+        .from('profilePics')
+        .list('default')
+
+    console.log(defaultProfilePics)
 
 }
