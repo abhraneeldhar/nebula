b0be21c5d1bb0753b49ad35be67a5ccc576a7928
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-17 17:42:31 +0530

    setupaccount get default notes

--- app/utils/setupNewAccount.ts
+++ app/utils/setupNewAccount.ts
@@ -1,6 +1,31 @@
+"use server"
 import { getUserDetails } from "./getUserDetails";
+import { mongoClientCS } from "./mongoConnector";
+import { v4 as uuidv4 } from "uuid";
+
 
 export async function setupNewAccount(userId:string){
     const userDetails= await getUserDetails(userId);
-    
+    if(userDetails.newAccount==false){
+        return(null)
+    }
+
+    await mongoClientCS.connect();
+    const db = mongoClientCS.db("notesApp");
+    const defaultNotes = db.collection("defaultNotes");
+    const defaultNotesArray = await defaultNotes.find({}).toArray();
+
+    // Modify the array here
+    const newDefaultNotes = defaultNotesArray.map(note => {
+        const { _id, ...rest } = note;
+        return {
+            ...rest,
+            id:uuidv4(),
+            owner: userId,
+            lastModifiedAt: Date.now()
+        };
+    });
+
+    console.log(newDefaultNotes);
+
 }
