38803cf0be957fcf6eb920cc09c81797b4f2c443
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-18 22:16:17 +0530

    hero section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
-"use server"
 import style from "./root.module.css"
+import Image from "next/image"
+import nebulaLogo from "../public/landingpage/nebulaLogo.png"
+import { ArrowRight } from "lucide-react"
+import { Button } from "@/components/ui/button"
 export default async function Main() {
 
   return (
     <div className={style.main}>
+      <Image className={style.cloudGif} src="/landingpage/cloudgif-1.gif" alt="" width={100} height={100} />
       <div className={style.tab}>
+        <Image src={nebulaLogo} alt="N" />
+        <div className={style.sectionTags}>
+          <a>Edit</a>
+          <a>Connect</a>
+          <a>Nebula</a>
+          <a>Personalize</a>
+          <a>About</a>
+        </div>
+      </div>
+      <div className={style.bigAghLogo}>
+        <h1>NEBULA</h1>
+        <Button className={style.getStarted}>Get Started <ArrowRight className={style.arrowRight} /></Button>
+      </div>
+      <div className={style.documentEditing}>
+        huzz
         
       </div>
+
     </div>
   )
 }
