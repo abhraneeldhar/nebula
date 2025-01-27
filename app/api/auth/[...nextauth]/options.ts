73897bf2d918d91315ccbf815854c77b155ad26e
abhraneeldhar <abhraneeldhar@gmail.com>
2025-01-27 23:05:43 +0530

    idk wtf was wrong with auth but it works not

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -8,10 +8,80 @@ import { mongoClientCS } from "@/app/utils/mongoConnector"
 import jwt from "jsonwebtoken"
 // import { userType } from "@/app/utils/fileFormat"
 import { v4 as uuidv4 } from "uuid";
+import { createClient } from "@supabase/supabase-js";
+import { getUserDetails } from "@/app/utils/getUserDetails";
 // import { userDetailsType } from "@/app/setupAccount/page"
 // import { supabase } from "@/app/utils/supabase/client"
 // import { defaultPfpUpdate } from "@/app/utils/defaultPfpUpdate"
-import { setupNewAccount } from "@/app/utils/setupNewAccount"
+// import { setupNewAccount } from "@/app/utils/setupNewAccount"
+
+async function setupNewAccount(userId: string) {
+    const userDetails = await getUserDetails(userId);
+    if (userDetails.newAccount == false) {
+        return (null)
+    }
+    console.log("userdetails: ",userDetails)
+
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    console.log("created supabase client")
+
+    // await mongoClientCS.connect();
+    const db = mongoClientCS.db("notesApp");
+    const defaultNotes = db.collection("defaultNotes");
+    const notesCollection=db.collection("notes");
+    const usersCollection=db.collection("users")
+    const defaultNotesArray = await defaultNotes.find({}).toArray();
+
+    // Modify the array here
+    const newDefaultNotes = defaultNotesArray.map(note => {
+        const { _id, ...rest } = note;
+        return {
+            ...rest,
+            id: uuidv4(),
+            owner: userId,
+            lastModifiedAt: Date.now()
+        };
+    });
+
+    const result = await notesCollection.insertMany(newDefaultNotes);
+    console.log("inserted default notes: ",result)
+    
+
+
+    const { data: defaultProfilePics, error: e1 } = await supabase
+        .storage
+        .from('profilePics')
+        .list('default')
+    if (defaultProfilePics) {
+        const newPicName = `profileImage${Date.now()}`
+        const randomPfp = defaultProfilePics[Math.floor(Math.random() * defaultProfilePics.length)]
+
+        const { data:copyData, error: e2 } = await supabase
+            .storage
+            .from('profilePics')
+            .copy(`default/${randomPfp.name}`, `${userId}/${newPicName}`)
+
+        console.log(copyData)
+
+        const { data: publicUrl } = supabase
+            .storage
+            .from('profilePics')
+            .getPublicUrl(`${userId}/${newPicName}`)
+
+
+        console.log("new pic url>>>>> ", publicUrl)
+
+        await usersCollection.updateOne({userId:userId},{$set:{imageUrl:publicUrl.publicUrl}})
+
+    }
+
+    await usersCollection.updateOne({userId:userId},{$set:{newAccount:false}})
+
+}
 
 export const options: NextAuthOptions = {
     // adapter: SupabaseAdapter({
