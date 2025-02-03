f19ddb648ad0077dfe14c38dec8fd49d7bf0448c
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-04 03:19:51 +0530

    trying catche 2

--- app/utils/getDisplayNotes.ts
+++ app/utils/getDisplayNotes.ts
@@ -1,6 +1,7 @@
 "use server"
 import { getOneNote } from "./getOneNote";
 import { mongoClientCS } from "./mongoConnector";
+import { Note } from "./fileFormat";
 
 export async function getDisplayNotes(userId: string) {
     await mongoClientCS.connect();
@@ -8,9 +9,12 @@ export async function getDisplayNotes(userId: string) {
     const notesCollection = db.collection("notes");
     const displayNotes = await notesCollection.find({ owner: userId }, { projection: { content: 0 } }).toArray();
     // console.log("display notes>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",displayNotes)
+    let catcheList:Note[]|null=[]
     displayNotes.forEach((note)=>{
         const getCatches=async()=>{
             const immaCatche= await getOneNote(userId,note.id as string);
+            // console.log(immaCatche);
+            catcheList.push(immaCatche);
         }
         getCatches()
     })
