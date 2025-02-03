c7c9af1cd8591d7becab46809087e926b79715fa
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-04 03:14:31 +0530

    trying to cache

--- app/utils/getDisplayNotes.ts
+++ app/utils/getDisplayNotes.ts
@@ -1,11 +1,18 @@
 "use server"
+import { getOneNote } from "./getOneNote";
 import { mongoClientCS } from "./mongoConnector";
 
-export async function getDisplayNotes(userId: string){
+export async function getDisplayNotes(userId: string) {
     await mongoClientCS.connect();
-        const db=mongoClientCS.db("notesApp");
-        const notesCollection=db.collection("notes");
-        const displayNotes= await notesCollection.find({ owner: userId },{projection:{content:0}}).toArray();
-        // console.log("display notes>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",displayNotes)
-        return (JSON.parse(JSON.stringify(displayNotes)))
+    const db = mongoClientCS.db("notesApp");
+    const notesCollection = db.collection("notes");
+    const displayNotes = await notesCollection.find({ owner: userId }, { projection: { content: 0 } }).toArray();
+    // console.log("display notes>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>",displayNotes)
+    displayNotes.forEach((note)=>{
+        const getCatches=async()=>{
+            const immaCatche= await getOneNote(userId,note.id as string);
+        }
+        getCatches()
+    })
+    return (JSON.parse(JSON.stringify(displayNotes)))
 }
