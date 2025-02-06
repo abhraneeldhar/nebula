d0e20f26ebd3b54e2222a9b77a15cc92816f8162
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-07 02:09:13 +0530

    fk it im starting over

--- app/settings/account/page.tsx
+++ app/settings/account/page.tsx
@@ -6,26 +6,43 @@ import pfp from "../../../public/pfp.jpeg"
 import banner from "../../../public/banner.jpg"
 import { Input } from "@/components/ui/input"
 
+import { ArrowLeft } from "lucide-react"
 
-export default function AccountsPage(){
-    return(<>
-    <div className={styles.main}>
-        <div className={styles.tab}></div>
-        <div className={styles.mainContent}>
-            <div className={styles.photosSection}>
-                <Image src={banner} alt=""/>
-                <Image src={pfp} alt=""/>
+
+export default function AccountsPage() {
+    return (<>
+        <div className={styles.main}>
+            <div className={styles.tab}>
+                <a href="/settings">
+                    <div className={styles.tabNameDiv}>
+                        <ArrowLeft />
+                        <h1>Back to settings</h1>
+                    </div>
+                </a>
             </div>
-
-            <div className={styles.detailsHolder}>
-                <div className={styles.detailCard}>
-                    <h1>Name</h1>
-                    <Input placeholder="meow"/>
+            <div className={styles.mainContent}>
+                <div className={styles.photosSection}>
+                    <Image src={banner} alt="" />
+                    <Image src={pfp} alt="" />
                 </div>
+
+                <div className={styles.detailsHolder}>
+                    <div className={styles.detailCard}>
+                        <h1>Name</h1>
+                        <Input placeholder="meow" />
+                    </div>
+                    <div className={styles.detailCard}>
+                        <h1>Username</h1>
+                        <Input placeholder="meow" />
+                    </div>
+                    <div className={styles.detailCard}>
+                        <h1>Bio</h1>
+                        <textarea placeholder="meoeoewmeow" />
+                    </div>
+                </div>
+
+
+
             </div>
-
-
-
-        </div>
-    </div></>)
+        </div></>)
 }
