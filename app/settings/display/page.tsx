a4de3ec7d6edbe23b659526095b91f8132ecd723
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-09 17:47:43 +0530

    added settingsbtn

--- app/settings/display/page.tsx
+++ app/settings/display/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { ArrowBigLeft } from "lucide-react"
+import { ArrowBigLeft, ArrowLeft } from "lucide-react"
 import styles from "./display.module.css"
 import { useRouter } from "next/navigation"
 import { Button } from "@radix-ui/themes";
@@ -81,51 +81,58 @@ export default function Displaysettings() {
             color4: "#71C9CE"
         }
     ];
-    
 
-    const PreviewCard = ({ theme }: { theme: {
-        name:string,
-        color1:string,
-        color2:string,
-        color3:string,
-        color4:string,
-    } }) => {
+
+    const PreviewCard = ({ theme }: {
+        theme: {
+            name: string,
+            color1: string,
+            color2: string,
+            color3: string,
+            color4: string,
+        }
+    }) => {
         let cardClassname: string;
         if (resolvedTheme == theme.name) {
-            cardClassname = `${styles.previewCard} ${styles.selectedCard}`
+            cardClassname = `${styles.previewCardMain} ${styles.selectedCard}`
         }
         else {
-            cardClassname = `${styles.previewCard}`
+            cardClassname = `${styles.previewCardMain}`
         }
         return (
-            <div onClick={()=>{setTheme(theme.name)}} style={{ backgroundColor: theme.color1 }} className={cardClassname}>
-                <div className={styles.sidebar}>
-                    <div style={{ backgroundColor:theme.color3 }} className={styles.sidebarDivs}></div>
-                    <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
-                    <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
-                    <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
+            <div className={cardClassname}>
+                <div onClick={() => { setTheme(theme.name) }} style={{ backgroundColor: theme.color1 }} className={styles.previewCard}>
+                    <div className={styles.sidebar}>
+                        <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
+                        <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
+                        <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
+                        <div style={{ backgroundColor: theme.color3 }} className={styles.sidebarDivs}></div>
+                    </div>
+                    <div className={styles.mainPreview}>
+                        <div style={{ backgroundColor: theme.color2 }} className={styles.thinDiv}></div>
+                        <div style={{ backgroundColor: theme.color4 }} className={styles.thiccDiv}></div>
+                    </div>
                 </div>
-                <div className={styles.mainPreview}>
-                    <div style={{ backgroundColor: theme.color2 }} className={styles.thinDiv}></div>
-                    <div style={{ backgroundColor: theme.color4 }} className={styles.thiccDiv}></div>
-                </div>
+                <h1>{theme.name}</h1>
             </div>
+
         )
     }
 
 
     return (<>
         <div className={styles.main}>
+            <div><ArrowLeft className={styles.backBtn} onClick={()=>{router.push("/settings")}}/></div>
             <h1 className={styles.pageHeader}>Display Settings</h1>
             <p className={styles.pagePara}>Choose your preffered interface apperance.</p>
 
             <div className={styles.previewCardHolder}>
 
                 {colorPalettes.map((theme) => (
-                    <PreviewCard theme={theme} />
+                    <PreviewCard key={theme.name} theme={theme} />
                 ))}
             </div>
-            
+
         </div>
     </>)
 }
