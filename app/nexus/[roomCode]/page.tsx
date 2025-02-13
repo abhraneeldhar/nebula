d966ad249443337f5d41e5b350475a9b5d010641
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-14 01:25:11 +0530

    added no room found

--- app/nexus/[roomCode]/page.tsx
+++ app/nexus/[roomCode]/page.tsx
@@ -1,8 +1,21 @@
 import ChatRoomComp from "@/app/_components/chatRoomComp/chatRoomComp";
+import { createClient } from "@supabase/supabase-js";
 
-export default async function ChatRoomPage({ params }: { params: Promise<{ roomCode: string }> }){
+export default async function ChatRoomPage({ params }: { params: Promise<{ roomCode: string }> }) {
     const roomCode = (await params).roomCode;
-    return(<>
-        <ChatRoomComp roomCode={roomCode}/>
-    </>)
+
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    const { data: checkRoom, error } = await supabase.from("chat_rooms").select().eq("roomcode", roomCode);
+    if (checkRoom?.length == 0) {
+        return (<>no room here mf</>)
+    }
+    else {
+        return (<>
+            <ChatRoomComp roomCode={roomCode} />
+        </>)
+    }
 }
