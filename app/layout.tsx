657454451e46c9def55a2f315a846f2c28dea1e3
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-14 13:30:54 +0530

    changed root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"/>
+
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
