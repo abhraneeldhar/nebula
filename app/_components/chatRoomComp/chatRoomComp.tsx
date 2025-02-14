f2f772266e25824790959b39570e4e81d9cc3ed2
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-14 18:23:20 +0530

    changed scroll function

--- app/_components/chatRoomComp/chatRoomComp.tsx
+++ app/_components/chatRoomComp/chatRoomComp.tsx
@@ -118,7 +118,7 @@ export default function ChatRoomComp({ roomCode }: { roomCode: string }) {
 
 
     useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth",block:"end" });
         console.log(messagesEndRef.current?.scrollHeight)
     }, [messageArray]);
 
