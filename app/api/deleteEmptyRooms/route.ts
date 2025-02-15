3df9eea92e5b4e78410212159e14ba11617b8f31
abhraneeldhar <abhraneeldhar@gmail.com>
2025-02-15 20:19:31 +0530

    changed function imports

--- app/api/deleteEmptyRooms/route.ts
+++ app/api/deleteEmptyRooms/route.ts
@@ -1,5 +1,5 @@
-"use server"
-import { deleteEmptyRooms } from "@/app/utils/nexus/cleanupRooms";
+import { supabase } from "@/app/utils/supabase/client";
+
 export async function GET(request: Request) {
     const authHeader = request.headers.get('authorization');
     if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
@@ -7,6 +7,53 @@ export async function GET(request: Request) {
         status: 401,
       });
     }
+
+    async function deleteRoom(roomcode: string) {
+      const response = await supabase
+          .from('chat_rooms')
+          .delete()
+          .eq('roomcode', roomcode)
+      // console.log(response);
+  }
+  
+
+    async function deleteEmptyRooms() {
+
+      var emptyRoomCodes: string[] = []
+  
+      const { data: chat_rooms, error } = await supabase
+          .from('chat_rooms')
+          .select()
+      if (chat_rooms) {
+          chat_rooms.forEach((room) => {
+              const channel = supabase.channel(room.roomcode);
+  
+              channel.subscribe(async (status) => {
+                  if (status === 'SUBSCRIBED') {
+                      await channel.track({ name: "voyager", status: 'online' });
+                  }
+              });
+  
+              channel.on('presence', { event: 'sync' }, () => {
+                  const presenceState = channel.presenceState();
+                  const usersArray = Object.values(presenceState).flatMap(userList => userList);
+                  if (usersArray.length == 0) {
+                      console.log("trynna delete ",room.roomcode);
+                      deleteRoom(room.roomcode);
+                      emptyRoomCodes.push(room.roomcode);
+                  }
+                  channel.unsubscribe();
+              });
+  
+  
+  
+          })
+      }
+      else {
+          console.log(error)
+      }
+  }
+
     await deleteEmptyRooms();
     return new Response("deleted empty rooms")
     
